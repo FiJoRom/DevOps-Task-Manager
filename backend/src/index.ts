e7866b15601786afa742f5d5bac6b9587e810a09
FiJoRom <finn.romeis@web.de>
2025-05-19 11:31:51 +0200

    Fix: 1/2 Backend Call funktioniert

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,15 @@ import fastifyCors from '@fastify/cors';
 const fastify = Fastify({ logger: true });
 
 fastify.register(fastifyCors, {
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:3000',
 });
 
+// Test ob Kommunikation Backend und Frontend funktioniert, Antwort auf Ping aus Frontend
 fastify.get('/api/ping', async () => {
-  return { pong: true };
+  return { message: 'pong' };
 });
 
-fastify.listen({ port: 3001 }, (err, address) => {
+fastify.listen({ port: 3001, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);

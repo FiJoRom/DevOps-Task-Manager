cf9ea7f7f04646baaaa67ab5eea099dc0005490b
Finn <101019678+FiJoRom@users.noreply.github.com>
2025-06-08 18:39:08 +0200

    fix: init test-result folder

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    reporters: ['default', 'junit'],
-    outputFile: {
-      junit: 'test-results/junit.xml',
-    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov'],
       reportsDirectory: './coverage',
+    },
+    reporters: ['default', 'junit'],
+    outputFile: {
+      junit: './test-results/junit.xml',
     },
   },
 })
